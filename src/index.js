29555f2c4a3ec1d1f20b905674d39ca16fee8c85
JamesAlexanderHill <jhill7177@gmail.com>
2021-04-12 13:12:20 +1000

    load maps in observable

--- src/index.js
+++ src/index.js
@@ -1,36 +1,47 @@
 import dotenv from 'dotenv';
 import { Loader } from '@googlemaps/js-api-loader';
+import { from, forkJoin, of } from 'rxjs';
+import { delay, map } from 'rxjs/operators';
 
 dotenv.config();
-const map = null;
+
+//Google map observable
 const googleMapsLoader = new Loader({
   apiKey: process.env.MAPS_API,
   version: "weekly",
   libraries: ["drawing"]
 }).load();
+const googleMaps$ = from(googleMapsLoader).pipe(map(() => {
+    const mapOptions = {
+        center: {
+            lat: -33.7813976,
+            lng: 151.102415
+        },
+        zoom: 11
+    };
+    const map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    return map
+}));
 
-const mapOptions = {
-  center: {
-    lat: -33.7809197,
-    lng: 150.9624176
-  },
-  zoom: 11
-};
+//Get data from database
+const centresData$ = of('Centres').pipe(delay(500));
+const coachesData$ = of('Coaches').pipe(delay(1000));
+const playersData$ = of('Players').pipe(delay(1500));
 
-googleMapsLoader
-  .then(() => {
-    map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    console.log("Map Loaded");
-  })
-  .then(() => {
-      addMarkers(map);
-      console.log("Markers Added");
-  })
-  .catch(e => {
-    // do something
-  });
+//Merge data and initiate map once everything has been resolved
+const data$ = forkJoin([googleMaps$, centresData$, coachesData$, playersData$]);
+data$.subscribe({
+    next: data => initMap(...data)
+});
 
-const addMarkers = (map) => {
+//initialise map
+const initMap = (map, centres, coaches, players) => {
+    // set Global params
+    // add markers
+    addMarkers(map, centres);
+}
+
+const addMarkers = (map, centres) => {
     console.log("Map: ", map)
     new google.maps.Marker({
         position: {lat: -33.7809197,lng: 150.9624176},
